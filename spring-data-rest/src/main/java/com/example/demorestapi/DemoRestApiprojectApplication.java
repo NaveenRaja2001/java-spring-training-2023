122326167abcf45854d738a0fddb5ea61e7dfb5e
NaveenRaja2001 <125632459+NaveenRaja2001@users.noreply.github.com>
2023-08-07 11:02:55 +0530

    spring-data-rest

---
+++ spring-data-rest/src/main/java/com/example/demorestapi/DemoRestApiprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demorestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoRestApiprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoRestApiprojectApplication.class, args);
+	}
+
+}
