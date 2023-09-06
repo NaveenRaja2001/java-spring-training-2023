f150f778f23be46773325f8e711d8a6221fc22a3
NaveenRaja2001 <125632459+NaveenRaja2001@users.noreply.github.com>
2023-09-06 15:24:19 +0530

    security-demo1

---
+++ springscurity/src/main/java/com/example/springscurity/springscurity/SpringscurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.springscurity.springscurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringscurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringscurityApplication.class, args);
+	}
+
+}
---
+++ springscurity/src/test/java/com/example/springscurity/springscurity/SpringscurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springscurity.springscurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringscurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
